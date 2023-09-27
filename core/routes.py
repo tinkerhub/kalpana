610ac0811e150b07e242d2dfd131fbe8128cbd85
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2023-09-27 12:31:39 +0530

    fix an issue

--- core/routes.py
+++ core/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from core.ai import query_result
+from core.ai import chat
 from core.models import Message
 
 
@@ -12,7 +12,7 @@ async def process_web_message(message: Message):
     chat_id = message.chat_id
     text = message.text
     history = history.get(chat_id, [])
-    response, new_history = query_result(text, history)
+    response, new_history = chat(text, history)
     history[chat_id] = new_history
     return {"message": response}
     
